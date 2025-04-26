0024202409356cf92ee0f14a1e614b808bd7d123
XiaoDev2003 <xiaotiensinh20102003@gmail.com>
2025-04-27 02:19:16 +0700

    Khởi tạo dự án

---
+++ frontend/src/services/axios/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost/Inkspire/backend/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+// Interceptor xử lý token xác thực
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
